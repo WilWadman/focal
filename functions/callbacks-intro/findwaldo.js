0c874d0de387343e70d5dde8cf8ea39fb664dab7
Will Wadman <wadmanwill@gmail.com>
2022-10-20 17:23:12 +0000

    find waldo activity

---
+++ functions/callbacks-intro/findwaldo.js
@@ -0,0 +1,35 @@
+// The second argument/parameter is expected to be a (callback) function
+/* const findWaldo = function(names, found) {
+  for (let i = 0; i < names.length; i++) {
+    let name = names[i];
+    if (name === "Waldo")
+      {
+      found(i);   // execute callback
+    }
+  }
+};
+
+const actionWhenFound = function(index) {
+
+  console.log("Found Waldo at index " + index + "!");
+};
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
+
+*/
+// The second argument/parameter is expected to be a (callback) function
+const findWaldo = function(names, found) {
+  names.forEach(function(element, index) {
+    if (element === "Waldo") {
+      found(index);
+    }
+  });
+};
+  
+
+const actionWhenFound = function(index) {
+
+  console.log("Found Waldo at index " + index + "!");
+};
+
+findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
